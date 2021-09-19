6f096f0ce29d4cb75cfdca84ba6bd374ada343d0
Sumon-miazi <sumonmiazi59@gmail.com>
2021-09-19 18:37:27 +0600

    #Added blank PaymentHistoryActivity

---
+++ app/src/main/java/com/itbeebd/cesc_nsl/activities/student/PaymentHistoryActivity.java
@@ -0,0 +1,16 @@
+package com.itbeebd.cesc_nsl.activities.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itbeebd.cesc_nsl.R;
+
+public class PaymentHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_history);
+    }
+}
