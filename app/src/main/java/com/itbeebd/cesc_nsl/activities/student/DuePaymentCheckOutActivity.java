6d920599e4b93df874e98abea31084538cfd67e3
Sumon-miazi <sumonmiazi59@gmail.com>
2021-09-23 12:35:28 +0600

    #Added back press functionality in DuePaymentCheckOutActivity

--- app/src/main/java/com/itbeebd/cesc_nsl/activities/student/DuePaymentCheckOutActivity.java
+++ app/src/main/java/com/itbeebd/cesc_nsl/activities/student/DuePaymentCheckOutActivity.java
@@ -1,7 +1,9 @@
 package com.itbeebd.cesc_nsl.activities.student;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,26 +14,23 @@ import com.itbeebd.cesc_nsl.api.ApiUrls;
 
 public class DuePaymentCheckOutActivity extends AppCompatActivity {
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_invoice);
 
-        WebView webView = findViewById(R.id.webview);
-
-     //   WebSettings webSettings = webView.getSettings();
-      //  webSettings.setJavaScriptEnabled(true);
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setInitialScale(100);
 
         if(getIntent().hasExtra("invoiceNo")){
             String url = ApiUrls.INVOICE_URL + getIntent().getStringExtra("invoiceNo");
 
-
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.getSettings().setUseWideViewPort(true);
-            webView.setInitialScale(100);
             webView.setWebViewClient(new WebViewClient() {
-//
+
 //                @Override
 //                public void onPageFinished(WebView view, String url)
 //                {
@@ -48,22 +47,46 @@ public class DuePaymentCheckOutActivity extends AppCompatActivity {
                             "(elem = document.getElementsByTagName('footer')[0]).parentNode.removeChild(elem); " +
                             "})()");
                 }
+
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                    if(url.contains(ApiUrls.BASE_URL)) return false;
+                    openExternalLinkInBrowser(url);
+                    return true;
+                }
             });
 
-           // String postData = null;
- //           try {
+                webView.loadUrl(url);
+
+        }
+    }
+
+    private void openExternalLinkInBrowser(String url){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        this.startActivity(intent);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(webView != null){
+            if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+                this.webView.goBack();
+                return true;
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
+
+// String postData = null;
+//           try {
 //                postData = "studentid=" + URLEncoder.encode("321005170", "UTF-8")
 //                        + "&password=" + URLEncoder.encode("123456", "UTF-8")
 //                        + "&fcm_token=" + URLEncoder.encode("156", "UTF-8");
-                webView.loadUrl(url);
-               // webView.postUrl(url,postData.getBytes());
+// webView.postUrl(url,postData.getBytes());
 //            } catch (UnsupportedEncodingException e) {
 //                e.printStackTrace();
 //            }
-
-        }
-
-
-
-    }
-}
--- app/src/main/java/com/itbeebd/cesc_nsl/api/ApiUrls.java
+++ app/src/main/java/com/itbeebd/cesc_nsl/api/ApiUrls.java
@@ -2,7 +2,7 @@ package com.itbeebd.cesc_nsl.api;
 
 public class ApiUrls {
 
+    public static final String BASE_URL = "http://192.168.1.251";
     public static final String BASE_IMAGE_URL = "http://192.168.1.251/cescms";
     public static final String INVOICE_URL =  BASE_IMAGE_URL + "/invoiceDetails/";
-    public static final String NEW_VOUCHER =  BASE_IMAGE_URL + "/invoiceDetails/";
 }
