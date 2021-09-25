93dda0ca59dc9e8d27d3b59c55f36d8d3afec0b4
Sumon-miazi <sumonmiazi59@gmail.com>
2021-09-25 10:38:11 +0600

    #Added blank LibraryBookActivity and single_library_book_view

---
+++ app/src/main/java/com/itbeebd/cesc_nsl/activities/student/LibraryBookActivity.java
@@ -0,0 +1,16 @@
+package com.itbeebd.cesc_nsl.activities.student;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.itbeebd.cesc_nsl.R;
+
+public class LibraryBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_book);
+    }
+}
