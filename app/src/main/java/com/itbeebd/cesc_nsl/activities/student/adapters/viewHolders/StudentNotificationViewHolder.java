6a64de228412f60e257f3834f2cf88b7410e294b
Sumon-miazi <sumonmiazi59@gmail.com>
2021-09-28 15:14:26 +0600

    #Made link clickable in Notification #Added drawer menu library

--- app/src/main/java/com/itbeebd/cesc_nsl/activities/student/adapters/viewHolders/StudentNotificationViewHolder.java
+++ app/src/main/java/com/itbeebd/cesc_nsl/activities/student/adapters/viewHolders/StudentNotificationViewHolder.java
@@ -2,13 +2,14 @@ package com.itbeebd.cesc_nsl.activities.student.adapters.viewHolders;
 
 import android.content.Context;
 import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.abdulhakeem.seemoretextview.SeeMoreTextView;
 import com.itbeebd.cesc_nsl.R;
 import com.itbeebd.cesc_nsl.activities.student.adapters.genericClasses.BaseViewHolder;
 import com.itbeebd.cesc_nsl.activities.student.adapters.genericClasses.OnRecyclerObjectClickListener;
@@ -16,7 +17,8 @@ import com.itbeebd.cesc_nsl.utils.NotificationObj;
 
 public class StudentNotificationViewHolder extends BaseViewHolder<NotificationObj, OnRecyclerObjectClickListener<NotificationObj>> {
     private final TextView notificationTitle;
-    private final SeeMoreTextView notificationBody;
+    private final TextView notificationBody;
+   // private final SeeMoreTextView notificationBody;
     private Context context;
 
     public StudentNotificationViewHolder(@NonNull View itemView, Context context) {
@@ -29,26 +31,19 @@ public class StudentNotificationViewHolder extends BaseViewHolder<NotificationOb
     @Override
     public void onBind(NotificationObj item, @Nullable OnRecyclerObjectClickListener<NotificationObj> listener) {
 
-        notificationBody.setTextMaxLength(80); //default is 250 charachters
-        notificationBody.expandText(false);
+   //     notificationBody.setTextMaxLength(80); //default is 250 charachters
+    //    notificationBody.expandText(false);
+
+     //   notificationTitle.setText(item.getTitle());
+    //    notificationBody.setContent(String.valueOf(Html.fromHtml(item.getBody())));
 
         notificationTitle.setText(item.getTitle());
-        notificationBody.setContent(String.valueOf(Html.fromHtml(item.getBody())));
-        System.out.println(">>>>>>> " + item.getBody());
+
+        Spanned policy = Html.fromHtml(item.getBody());
+        notificationBody.setText(policy);
+        notificationBody.setMovementMethod(LinkMovementMethod.getInstance());
+
        // notificationBody.toggle();
-        notificationBody.setSeeMoreTextColor(R.color.blue_primary); //default is #3F51B5
-/*
-        if(item.getImage() != null){
-            Glide.with(context)
-                    .load(ApiUrls.BASE_IMAGE_URL + item.getImage())
-                    .into(tempImageView);
-        }
-
-
-        tempName.setOnClickListener(view -> {
-            assert listener != null;
-            listener.onItemClicked(item, view);
-        });
- */
+      //  notificationBody.setSeeMoreTextColor(R.color.blue_primary); //default is #3F51B5
     }
 }
